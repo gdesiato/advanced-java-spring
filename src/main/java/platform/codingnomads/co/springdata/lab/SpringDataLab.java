ec37ac31ef09812923db58b9465379d927e8c7cf
Giuseppe Desiato <giodesiato@gmail.com>
2022-10-27 15:44:03 -0400

    spring data lab

--- src/main/java/platform/codingnomads/co/springdata/lab/SpringDataLab.java
+++ src/main/java/platform/codingnomads/co/springdata/lab/SpringDataLab.java
@@ -5,9 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import platform.codingnomads.co.springdata.lab.models.Area;
+import platform.codingnomads.co.springdata.lab.models.Route;
 import platform.codingnomads.co.springdata.lab.repositories.AreaRepository;
 import platform.codingnomads.co.springdata.lab.repositories.RouteRepository;
-import platform.codingnomads.co.springdata.lab_complete.models.Route;
+import platform.codingnomads.co.springdata.lab_complete.models.PointOfInterest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,8 +17,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SpringDataLab implements CommandLineRunner {
 
-    private final AreaRepository areaRepository;
-    private final RouteRepository routeRepository;
+    private final AreaRepository areaRepository1;
+    private final RouteRepository routeRepository1;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataLab.class);
@@ -25,19 +26,6 @@ public class SpringDataLab implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-//        final List<Area> areas = areaRepository.saveAll(
-//                Arrays.asList(
-//                        Area.builder().code("G").build(),
-//                        Area.builder().code("H").build(),
-//                        Area.builder().code("Y").build(),
-//                        Area.builder().code("Z").build(),
-//                        Area.builder().code("A").build(),
-//                        Area.builder().code("B").build(),
-//                        Area.builder().code("C").build(),
-//                        Area.builder().code("D").build()
-//                )
-//        );
 
 
 
@@ -51,50 +39,99 @@ public class SpringDataLab implements CommandLineRunner {
                         Area.builder().code("L").build(),
                         Area.builder().code("M").build()
                 ).forEach(area -> {
-                    if (!areaRepository.existsByCode(area.getCode())){
-                        areaRepository.save(area);
+                    if (!areaRepository1.existsByCode(area.getCode())){
+                        areaRepository1.save(area);
                     }
                 });
 
-//        Area a = areaRepository.findAreaByCode("D");
-//        Area b = areaRepository.findAreaByCode("E");
-//        Area c = areaRepository.findAreaByCode("F");
-//        Area d = areaRepository.findAreaByCode("G");
-//        Area e = areaRepository.findAreaByCode("H");
-//        Area f = areaRepository.findAreaByCode("I");
+        Area a = areaRepository1.findAreaByCode("D");
+        Area b = areaRepository1.findAreaByCode("E");
+        Area c = areaRepository1.findAreaByCode("F");
+        Area d = areaRepository1.findAreaByCode("G");
+        Area e = areaRepository1.findAreaByCode("H");
+        Area f = areaRepository1.findAreaByCode("I");
 
 
-//                    Arrays.asList(
-//                            Route.builder().origin(areaRepository.findAreaByCode("D"))
-//                                    .destination(areaRepository.findAreaByCode("E")).build(),
-//                            Route.builder().origin(areaRepository.findAreaByCode("F"))
-//                                    .destination(areaRepository.findAreaByCode("G")).build(),
-//                            Route.builder().origin(areaRepository.findAreaByCode("H"))
-//                                    .destination(areaRepository.findAreaByCode("I")).build(),
-//                            Route.builder().origin(areaRepository.findAreaByCode("L"))
-//                                    .destination(areaRepository.findAreaByCode("M")).build()
-//                    ).forEach(route -> {
-//                        if (!routeRepository.existsByCode(route.getCode())){
-//                            routeRepository.save(route);
-//                        }
-//                    });
+                    Arrays.asList(
+                            platform.codingnomads.co.springdata.lab.models.Route.builder().origin(areaRepository1.findAreaByCode("D"))
+                                    .destination(areaRepository1.findAreaByCode("E")).build(),
+                            platform.codingnomads.co.springdata.lab.models.Route.builder().origin(areaRepository1.findAreaByCode("F"))
+                                    .destination(areaRepository1.findAreaByCode("G")).build(),
+                            platform.codingnomads.co.springdata.lab.models.Route.builder().origin(areaRepository1.findAreaByCode("H"))
+                                    .destination(areaRepository1.findAreaByCode("I")).build(),
+                            Route.builder().origin(areaRepository1.findAreaByCode("L"))
+                                    .destination(areaRepository1.findAreaByCode("M")).build()
+                    ).forEach(route -> {
+                        if (!routeRepository1.existsByCode(route.getCode())){
+                            routeRepository1.save(route);
+                        }
+                    });
+
+                    Route route = Route.builder().origin(a).destination(b).build();
+                    Route route1 = new Route(a, b);
+                    routeRepository1.save(route1);
+
+                    Route route2 = new Route(c, d);
+                    routeRepository1.save(route2);
+
+                    Route route3 = new Route(e, f);
+                    routeRepository1.save(route3);
 
 
+          System.out.println(routeRepository1.findRouteByParam("D-E"));
 
 
-        // Route route = Route.builder().origin(a).destination(b).build();
-//        Route route1 = new Route(a, b);
-//        routeRepository.save(route1);
+//        if (pointOfInterestRepository.findAll().size() == 0) {
 //
-//        Route route2 = new Route(c, d);
-//        routeRepository.save(route2);
+//            PointOfInterest poi1 = new PointOfInterest("Gas Station", "Buc-ee's",
+//                    areaRepository1.findByCode("A"));
+//            // if a poi has an associated area, it may have routes as well
+//            poi1.addRoutes(routeRepository1.findAllByCodeContaining("A"));
 //
-//        Route route3 = new Route(e, f);
-//        routeRepository.save(route3);
-
-
-        //System.out.println(routeRepository.findRouteByParam("D-E"));
+//            PointOfInterest poi2 = new PointOfInterest("Gas Station", "Shell",
+//                    areaRepository1.findByCode("Z"));
+//            poi2.addRoutes(routeRepository1.findAllByCodeContaining("Z"));
+//
+//            PointOfInterest poi3 = new PointOfInterest("Restaurant", "Brick House",
+//                    areaRepository1.findByCode("O"));
+//            poi3.addRoutes(routeRepository1.findAllByCodeContaining("O"));
+//
+//            PointOfInterest poi4 = new PointOfInterest("Restaurant", "Nirmanz",
+//                    areaRepository1.findByCode("L"));
+//            poi4.addRoutes(routeRepository1.findAllByCodeContaining("L"));
+//
+//            // a poi can be associated with a route only, not a particular area (somewhere between 2 areas)
+//            PointOfInterest poi5 = new PointOfInterest("Roadside Attraction", "Ufo",
+//                    routeRepository1.findByCode("B-A"));
+//            PointOfInterest poi6 = new PointOfInterest("Roadside Attraction", "Waterfall",
+//                    routeRepository1.findByCode("H-Y"));
+//
+//            final List<PointOfInterest> poiList = pointOfInterestRepository.saveAll(
+//                    List.of(poi1, poi2, poi3, poi4, poi5, poi6)
+//            );
+//        }
+//
+//        System.out.println(pointOfInterestRepository.findAllByArea_code("A"));
+//        System.out.println(pointOfInterestRepository.findAllDistinctByRoutes_codeContaining("A"));
+//
+//        System.out.println(areaRepository1.findAllByPointsOfInterest_typeIgnoreCase("gas station"));
+//        System.out.println(areaRepository1.findByCode("A"));
+//
+//        System.out.println(routeRepository1.findByOrigin_code("T"));
+//        System.out.println(routeRepository1.findByOrigin_code("Y"));
+//        System.out.println(routeRepository1.findByOrigin_code("H"));
+//
+//        System.out.println(routeRepository1.findByDestination_code("R"));
+//        System.out.println(routeRepository1.findByDestination_code("Y"));
+//        System.out.println(routeRepository1.findByDestination_code("Z"));
+//
+//        System.out.println(routeRepository1.findByCode("T-Z"));
+//        System.out.println(routeRepository1.findByCode("H-Z"));
+//        System.out.println(routeRepository1.findByCode("L-O"));
 
 
     }
 }
+
+
+
